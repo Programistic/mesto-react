459a664754a8d200cac5f41f2a509e9ce5bc36df
Maksim Dav <maksim.dav@mail.ru>
2022-04-26 21:08:36 +0800

    delete empty tags and file Api.test.js

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -10,20 +10,18 @@ class PopupWithForm extends React.Component {
     const popupOpened = this.props.isOpen ? 'popup_opened' : false;
   
     return (
-      <>
-        <div className={`popup popup_role_${this.props.name} ${popupOpened}`}>
-          <div className="popup__container">
-            <h2 className="popup__heading">{this.props.title}</h2>
-            <form className="form" name={this.props.name}>
-              { this.props.children === 'edit' && <FieldsetPopupEdit /> }
-              { this.props.children === 'create' && <FieldsetPopupCreate /> }
-              { this.props.children === 'avatar-update' && <FieldsetPopupAvatarUpdate /> }   
-              <button className="form__submit button" type="submit" aria-label={this.props.buttonText}>{this.props.buttonText}</button>
-            </form>
-          </div>
-          <button className="button popup__button-close" type="button" aria-label="Закрыть" onClick={this.props.onClose}></button>
+      <div className={`popup popup_role_${this.props.name} ${popupOpened}`}>
+        <div className="popup__container">
+          <h2 className="popup__heading">{this.props.title}</h2>
+          <form className="form" name={this.props.name}>
+            { this.props.children === 'edit' && <FieldsetPopupEdit /> }
+            { this.props.children === 'create' && <FieldsetPopupCreate /> }
+            { this.props.children === 'avatar-update' && <FieldsetPopupAvatarUpdate /> }   
+            <button className="form__submit button" type="submit" aria-label={this.props.buttonText}>{this.props.buttonText}</button>
+          </form>
         </div>
-      </>
+        <button className="button popup__button-close" type="button" aria-label="Закрыть" onClick={this.props.onClose}></button>
+      </div>
     );
   }
 }
