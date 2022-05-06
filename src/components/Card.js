14ad3ae152a5814c117024244004e1e1ee8d90d2
Maksim Dav <maksim.dav@mail.ru>
2022-05-06 23:09:55 +0800

    use context in Main and Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 class Card extends React.Component {
+
+  static contextType = CurrentUserContext;
   
   handleClick = () => {
     this.props.onCardClick(this.props);
   } 
 
   render() {
+
+    const isOwn = this.props.owner._id === this.context._id;
+    const cardDeleteButtonClassName = (isOwn ? 'card__button-delete' : 'card__button-delete_inactive');
+
     return (
       <li className="card">
         <img className="card__image" src={this.props.link} alt={this.props.title} onClick={this.handleClick} />
-        <button className="button card__button-delete" type="button" aria-label="Удалить"></button>
+        <button className={`button {cardDeleteButtonClassName}`} type="button" aria-label="Удалить"></button>
         <div className="card__inner-container">
           <h2 className="card__title">{this.props.title}</h2>
             <button className="button card__button-like" type="button" aria-label="Симпатия">
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,26 +2,26 @@ import React from 'react';
 import '../index.css';
 import api from '../utils/Api.js';
 import Card from './Card.js';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = { userName: null, userDescription: null, userAvatar: null, cards: [] };
-  }  
+  }
+  
+  static contextType = CurrentUserContext;
 
   componentDidMount() {
     Promise.all([api.getProfile(), api.getCards()])
       .then(([userData, cardsData]) => {
         this.setState(
           {
-            userName: userData.name,
-            userDescription: userData.about,
-            userAvatar: userData.avatar,
             cards: cardsData,
           }
         );
         this.cardList = this.state.cards.map((card) => (
-          <Card key={card._id} title={card.name} link={card.link} number={card.likes.length} onCardClick={this.props.onCardClick} />
+          <Card key={card._id} title={card.name} link={card.link} number={card.likes.length} owner={card.owner._id} onCardClick={this.props.onCardClick} />
         ))
       })
       .catch((err) => {
@@ -33,13 +33,13 @@ class Main extends React.Component {
     return (
       <main>
         <section className="profile">
-          <img className="profile__avatar" src={this.state.userAvatar} alt="Аватар"/>
+          <img className="profile__avatar" src={this.context.avatar} alt="Аватар"/>
           <div className="profile__edit-icon" onClick={this.props.onEditAvatar}></div>    
           <div className="profile__inner-container">
-            <h1 className="profile__user-name">{this.state.userName}</h1>
+            <h1 className="profile__user-name">{this.context.name}</h1>
             <button className="button profile__button-edit" type="button" aria-label="Редактировать" onClick={this.props.onEditProfile}></button>
           </div>
-          <p className="profile__user-info">{this.state.userDescription}</p>
+          <p className="profile__user-info">{this.context.about}</p>
           <button className="button profile__button-add" type="button" aria-label="Добавить" onClick={this.props.onAddPlace}></button> 
         </section>
   
